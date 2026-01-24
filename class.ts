64fccc766b7ca0ba144b625863f7462aa09f73dc
rohit1302 <rohitnew1302@gmail.com>
2026-01-24 23:46:41 +0530

    done with class

---
+++ class.ts
@@ -0,0 +1,16 @@
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet(): void {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+
+const p1 = new Person("Rohit", 21);
+p1.greet();
